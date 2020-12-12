6d0619b462bba1d0b8032e37777d79b6e4bc4b26
v.potemskyi <volodum@gmail.com>
2020-12-12 16:58:28 +0200

    11

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -1,24 +1,30 @@
 package Tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import org.apache.log4j.Logger;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import pageObjects.HomePage;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
+import static utils.Log.debug;
+import static utils.Log.intLog;
 
 public class BaseTest extends BaseTestConfiguration {
 
     public static HomePage homePage;
-    public static Logger logger = Logger.getLogger(BaseTest.class);
+
+    @BeforeSuite()
+    public void initLogger() {
+        intLog();
+        debug("Logger is ready for using");
+    }
 
     @BeforeTest()
     public HomePage login() {
         homePage = open(Configuration.baseUrl, HomePage.class);
-        logger.debug("Home page opened");
+        debug("Home page opened");
         return homePage;
     }
 
--- src/test/java/Tests/BaseTestConfiguration.java
+++ src/test/java/Tests/BaseTestConfiguration.java
@@ -4,15 +4,16 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.LoggerFactory;
+import pageObjects.FinalResultTable;
 
 public abstract class BaseTestConfiguration {
-
     static {
-        Configuration.timeout = 6000;
+        Configuration.timeout = 90000;
         Configuration.browser = WebDriverRunner.CHROME;
         Configuration.browserSize = "1366x768";
         Configuration.pollingInterval = 200;
         Configuration.baseUrl = "http://www.it-league.lviv.ua";
-        PropertyConfigurator.configure("log4j.properties");
+        PropertyConfigurator.configure("src/main/resources/log4j.properties");
     }
 }
